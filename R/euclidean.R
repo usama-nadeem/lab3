8ddd8f0197a7457fc40868e405bded0c4f737d47
Usama Nadeem <usamanadeem321@gmail.com>
2022-09-13 22:32:48 +0200

    export added in euclidean

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,3 +1,13 @@
+#' @description 
+#' Find euclidean dist between 2 numbers
+#' 
+#' @param Num1,Num2 Two numbers for which euclidean is to be calculated
+#' @return The euclidean dist of `Num1` and `Num2`.
+#' @examples
+#' euclidean(1, 1)
+#' euclidean(10, 1)
+#' @export
+
 euclidean <-
 function (Num1, Num2)
 {
