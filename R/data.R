ad4a0e4c21b2765a1ca7a34aa43d276d6ced96a4
Usama Nadeem <usamanadeem321@gmail.com>
2022-09-13 22:25:12 +0200

    added data and its documentation

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' A wikipedia graph dataset
+#'
+#' A dataset containing the plot for a graph from wikipedia page
+#' diamonds.
+#'
+#' @format 18 objects, 3 variables
+#' \describe{
+#'   \item{v1}{price, in US dollars}
+#'   \item{v2}{weight of the diamond, in carats}
+#'   \item{w}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra}
+"wiki_graph"
